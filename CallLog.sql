6be11b4f80db4ae5eb9a3bbded9dce3b17561bee
aaronlar <aaronlar@buffalo.edu>
2023-05-06 21:38:52 -0400

    Dumped SQL database to CallLog.sql

---
+++ CallLog.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
+--
+-- Host: localhost    Database: CallLog
+-- ------------------------------------------------------
+-- Server version	8.0.32-0ubuntu0.22.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Calls`
+--
+
+DROP TABLE IF EXISTS `Calls`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Calls` (
+  `CallID` int NOT NULL AUTO_INCREMENT,
+  `Time` varchar(30) NOT NULL,
+  `Date` varchar(30) DEFAULT NULL,
+  `Out_or_In` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`CallID`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Calls`
+--
+
+LOCK TABLES `Calls` WRITE;
+/*!40000 ALTER TABLE `Calls` DISABLE KEYS */;
+INSERT INTO `Calls` VALUES (1,'2029','5/5/2023','In'),(2,'2032','5/5/2023','Out'),(3,'2033','5/5/2023','In'),(4,'2034','5/5/2023','Out'),(5,'2303','5/5/2023','Out'),(6,'2304','5/5/2023','Out'),(7,'2307','5/5/2023','In'),(8,'2308','5/5/2023','Out'),(9,'1539','5/6/2023','Out'),(10,'1545','5/6/2023','In'),(11,'1546','5/6/2023','Out'),(12,'1550','5/6/2023','In'),(13,'2043','5/6/2023','In'),(14,'2106','5/6/2023','Out'),(15,'2113','5/6/2023','Out'),(16,'2115','5/6/2023','In');
+/*!40000 ALTER TABLE `Calls` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-06 21:38:17
